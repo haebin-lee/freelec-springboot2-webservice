ef3ea43b27ae9aaf4907657c7e083304092db6f4
haebin.lee <luckylucy0717@gmail.com>
2020-12-15 23:21:01 +0900

    profile추가(real1, real2)

--- src/test/java/com/lucy/book/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/lucy/book/springboot/web/ProfileControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import sun.reflect.annotation.ExceptionProxy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
